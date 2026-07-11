e5de15450e235c698bad7d8c4fddd4431e741abd
Md_Younus_19 <mohammedyounish1911@gmail.com>
2026-07-11 22:33:27 +0530

    Add files via upload

--- Python/2. Odd or Even.py
+++
@@ -1,7 +0,0 @@
-userInput = int(input("Enter a Number to check Odd or Even:"))
-
-if( userInput % 2 == 0 ):
-    print(userInput, "is Even")
-
-else:
-    print(userInput, "is Odd")
--- Python/4. Sum of N natural numbers.py
+++
@@ -1,7 +0,0 @@
-userInput = int(input("Enter the Length of digits to Add:"))
-sum = 0
-
-for i in range(0, userInput+1):
-    sum +=i
-
-print(sum)
