8bb4cafb72845c97ef598c301504a8eae8e1331f
Mohammed Younish R M <mohammedyounish1911@gmail.com>
2026-07-28 20:27:52 +0530

    Solved Array Problems

---
+++ Python/03. Code Related to Arrays/66. Finding Non Repeating elements in an Array.py
@@ -0,0 +1,19 @@
+lengthOfArray = int(input("Enter the length of an Array:"))
+array = []
+
+for i in range(lengthOfArray):
+    element = input(f"Enter {i+1} element :")
+    array.append(element)
+    
+nonrepeat = []
+
+for i in range(lengthOfArray):
+    unique = True
+    for j in range(lengthOfArray):
+        if (( i != j ) and (array[i] == array[j])):
+            unique = False
+            break
+
+    if unique:
+        nonrepeat.append(array[i])
+print("The non repeated elements are", nonrepeat)
