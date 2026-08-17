c7658517b328cc9819682a2e71fe9e4bc4773342
Mohammed Younish R M <mohammedyounish1911@gmail.com>
2026-08-17 17:39:54 +0530

    Solved Array Problem

--- Python/03. Code Related to Arrays/71. Balanced Parenthesis Problem.py
+++ Python/03. Code Related to Arrays/71. Balanced Parenthesis Problem.py
@@ -1,20 +1,20 @@
 string = input("Enter the brackets:")
 
 while( len(string) != 0 ):
-        original = len(string)
+   original = len(string)
 
-        string = string.replace('(','')
-        string = string.replace('[','')
-        string = string.replace('{','')
-        string = string.replace(')','')
-        string = string.replace(']','')
-        string = string.replace('}','')
+   string = string.replace('(','')
+   string = string.replace('[','')
+   string = string.replace('{','')
+   string = string.replace(')','')
+   string = string.replace(']','')
+   string = string.replace('}','')
 
-        if( len(string) == original ):
-             break
+   if( len(string) == original ):
+      break
 
 if ( len(string) == 0):
    print("The Entered String has Balanced brackets")
    
 else:
-    print("The entered string does not have balanced brackets")
+   print("The entered string does not have balanced brackets")
