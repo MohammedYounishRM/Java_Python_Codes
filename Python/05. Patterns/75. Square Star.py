61ef6f5c724921fece6aecb65c44008231982dc7
Mohammed Younish R M <mohammedyounish1911@gmail.com>
2026-09-18 10:52:19 +0530

    Pattern Programs

--- Python/05. Patterns/75. Square Star.py
+++ Python/05. Patterns/75. Square Star.py
@@ -1,5 +1,5 @@
 inp = int(input("Enter the number:"))
 for i in range(inp):
     for j in range(inp):
-        print("*",end=" ")
+        print("*", end=" ")
     print()
---
+++ Python/05. Patterns/76. Rectangle star.py
@@ -0,0 +1,7 @@
+r = int(input("Enter the no of row : "))
+c = int(input("Enter the no of column : "))
+
+for i in range(r):
+    for j in range(c):
+        print("*", end=" ")
+    print()
